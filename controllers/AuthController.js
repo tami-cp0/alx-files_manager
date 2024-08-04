166fa5179224e6f7f119b99c2b37798f902557f0
tami-cp0 <findtamilore>
2024-08-04 15:59:40 +0100

    Basic auth completed

---
+++ controllers/AuthController.js
@@ -0,0 +1,72 @@
+import sha1 from 'sha1';
+import { ObjectId } from 'mongodb';
+import { v4 as uuidv4 } from 'uuid';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AuthController {
+  static async getConnect(req, res) {
+    // extract the email and password from the header
+    const authHeader = req.get('Authorization').split(' ');
+    if (!authHeader) {
+      return res.status(401).json({ error: 'Missing Authorization header' });
+    }
+
+    const authString = authHeader.pop();
+    const credential = Buffer.from(authString, 'base64').toString('utf-8');
+
+    // find the first occurence of :
+    const index = credential.indexOf(':');
+    if (index === -1) {
+      return res.status(400).json({ error: 'Malformed Authorization header' });
+    }
+
+    const email = credential.slice(0, index);
+    const sha1Password = sha1(credential.slice(index + 1));
+
+    try {
+      const user = await dbClient.users.findOne({ email, password: sha1Password });
+      console.log(user);
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+
+    const token = uuidv4();
+    const authToken = `auth_${token}`;
+    const duration = 24 * 3600;
+
+    // Get the user id
+    const id = (await dbClient.users.findOne({ email }))._id;
+
+    await redisClient.set(authToken, id.toString(), duration);
+    return res.status(200).json({ token });
+  }
+
+  static async getDisconnect(req, res) {
+    // Retrieve token from request headers
+    const token = req.get('X-Token');
+    if (!token) {
+      return res.status(400).json({ error: 'Token missing' });
+    }
+
+    // Get user ID from Redis using token
+    const id = new ObjectId(await redisClient.get(`auth_${token}`));
+    if (!id) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    // Find user in the database
+    const user = await dbClient.users.findOne({ _id: id });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    await redisClient.del(`auth_${token}`);
+    return res.status(204).send();
+  }
+}
+
+export default AuthController;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const router = Router();
 
+// Returns the status of the redis and mongodb server
 router.get('/status', AppController.getStatus);
+
+// Returns the number of users and files
 router.get('/stats', AppController.getStats);
+
+// Get all users
+router.post('/users', UsersController.postNew);
+
+// Gets the current user
+router.get('/users/me', UsersController.getMe);
+
+// connect the user to the server
+router.get('/connect', AuthController.getConnect);
+
+// disconnect the user from the server
+router.get('/disconnect', AuthController.getDisconnect);
 
 export default router;
